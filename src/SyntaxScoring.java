0789cf3401360675c7721cfabb7d26617ae5d7ba
Praveen John <praveenjohn50@gmail.com>
2022-11-09 17:33:48 +0100

    Code Optimization
         - Add input as a text file
         - Problem description added in Question.txt
         - Print both the results for part 1 and 2
         - Code cleanup

--- src/SyntaxScoring.java
+++ src/SyntaxScoring.java
@@ -1,110 +1,96 @@
 
+import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
 public class SyntaxScoring {
 
-    public static final String INPUT = "[({(<(())[]>[[{[]{<()<>>\n" +
-            "[(()[<>])]({[<{<<[]>>(\n" +
-            "{([(<{}[<>[]}>{[]{[(<()>\n" +
-            "(((({<>}<{<{<>}{[]{[]{}\n" +
-            "[[<[([]))<([[{}[[()]]]\n" +
-            "[{[{({}]{}}([{[{{{}}([]\n" +
-            "{<[[]]>}<{[{[{[]{()[[[]\n" +
-            "[<(<(<(<{}))><([]([]()\n" +
-            "<{([([[(<>()){}]>(<<{{\n" +
-            "<{([{{}}[<[[[<>{}]]]>[]]";
+    public static void main(String[] args) throws IOException {
 
-
-    public static void main(String[] args) {
-
-        Stack<String> charStack = new Stack<>();
+        Stack<String> charStack;
 
         int count = 0;
         List<BigInteger> scoreTotal = new ArrayList<>();
 
-        for (String line : INPUT.split("\n")) {
-            String[] charArr = line.trim().split("");
+
+        // Read input from input.txt file
+        File file = new File("C:\\Users\\prave\\IdeaProjects\\CodingTest\\src\\input.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String stringFromFile;
+        while ((stringFromFile = br.readLine()) != null) {
+            String[] charArr = stringFromFile.trim().split("");
 
             boolean isValid = true;
 
             charStack = new Stack<>();
+            /*Solution for Part 1 starts here*/
             mainForLoop:
             for (String c : charArr) {
                 switch (c) {
-                    case "(":
-                    case "[":
-                    case "{":
-                    case "<":
-                        charStack.push(c);
-                        break;
+                    case "(", "[", "{", "<" -> charStack.push(c);
 
-                    case ")":
+                    case ")" -> {
                         if (!charStack.peek().equals("(")) {
                             count += 3;
                             isValid = false;
                             break mainForLoop;
                         }
                         charStack.pop();
-                        break;
+                    }
 
-                    case "]":
+                    case "]" -> {
                         if (!charStack.peek().equals("[")) {
                             count += 57;
                             isValid = false;
                             break mainForLoop;
                         }
                         charStack.pop();
-                        break;
+                    }
 
-                    case "}":
+                    case "}" -> {
                         if (!charStack.peek().equals("{")) {
                             count += 1197;
                             isValid = false;
                             break mainForLoop;
                         }
                         charStack.pop();
-                        break;
+                    }
 
-                    case ">":
+                    case ">" -> {
                         if (!charStack.peek().equals("<")) {
                             count += 25137;
                             isValid = false;
                             break mainForLoop;
                         }
                         charStack.pop();
-                        break;
+                    }
                 }
             }
+            /*Solution for Part 1 Ends here*/
 
+            /*Solution for Part 2 starts here*/
             if (isValid) {
                 BigInteger countValid = BigInteger.ZERO;
 
                 while (charStack.size() > 0) {
                     String s = charStack.pop();
                     countValid = countValid.multiply(new BigInteger("5"));
-                    switch (s) {
-                        case "(":
-                            countValid = countValid.add(new BigInteger("1"));
-                            break;
-                        case "[":
-                            countValid = countValid.add(new BigInteger("2"));
-                            break;
-                        case "{":
-                            countValid = countValid.add(new BigInteger("3"));
-                            break;
-                        case "<":
-                            countValid = countValid.add(new BigInteger("4"));
-                            break;
-                    }
+                    countValid = switch (s) {
+                        case "(" -> countValid.add(new BigInteger("1"));
+                        case "[" -> countValid.add(new BigInteger("2"));
+                        case "{" -> countValid.add(new BigInteger("3"));
+                        case "<" -> countValid.add(new BigInteger("4"));
+                        default -> countValid;
+                    };
                 }
                 scoreTotal.add(countValid);
             }
+            /*Solution for Part 2 Ends here*/
         }
 
         Collections.sort(scoreTotal);
-
-        System.out.println(scoreTotal.get(Math.floorDiv(scoreTotal.size(), 2)));
+        System.out.println("Part 1 :" + count);
+        System.out.println("Part 2 :" + scoreTotal.get(Math.floorDiv(scoreTotal.size(), 2)));
 
     }
 
